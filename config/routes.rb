86ac5b3cb95bde575c52ac4bea9056e46cafd97a
laiba <laiba.asif@xprolabs.com>
2025-07-01 16:31:32 +0500

    handled error regarding user path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,4 @@ Rails.application.routes.draw do
 
   root "articles#index"
   get "up" => "rails/health#show", as: :rails_health_check
-  get "/users/:id", to: "users#show"
-  get "/users/:id", to: "users#show", as: "user"
 end
