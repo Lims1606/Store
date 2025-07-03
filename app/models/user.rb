8e30252eaeba83d5501452465bf86a3df53acff0
laiba <laiba.asif@xprolabs.com>
2025-07-03 20:10:23 +0500

    practiced-models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  after_create :log_new_user
+
+  private
+
+  def log_new_user
+    puts "A new user was registered"
+  end
 end
