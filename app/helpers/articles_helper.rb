13a98fde43ea94b23f13c877398d06d2caf0d64d
laiba <laiba.asif@xprolabs.com>
2025-07-22 19:59:56 +0500

    Practiced the action views

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,10 @@
 module ArticlesHelper
+  def short_description(article, length = 10)
+    if article.description.length > length
+      truncate(article.description, length: length) + " " +
+      link_to("Read more...", article_path(article))
+    else
+      article.description
+    end
+  end
 end
